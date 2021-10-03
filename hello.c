cc5d947996f9ce834bfe0948054f357f66c4aa82
Vinz2008 <clubfunreading@gmail.com>
2021-10-03 13:42:45 +0200

    first commit

---
+++ hello.c
@@ -0,0 +1,28 @@
+#include <efi.h>
+#include <efilib.h>
+ 
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+    EFI_STATUS Status;
+    EFI_INPUT_KEY Key;
+ 
+    
+    ST = SystemTable;
+ 
+    /
+    Status = ST->ConOut->OutputString(ST->ConOut, L"Hello World\r\n"); // EFI Applications use Unicode and CRLF, a la Windows
+    if (EFI_ERROR(Status))
+        return Status;
+ 
+    
+    Status = ST->ConIn->Reset(ST->ConIn, FALSE);
+    if (EFI_ERROR(Status))
+        return Status;
+ 
+    /* Now wait until a key becomes available.  This is a simple
+       polling implementation.  You could try and use the WaitForKey
+       event instead if you like */
+    while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ;
+ 
+    return Status;
+}
